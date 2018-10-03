9ba0f6986eb0849d73f85c726da4746d5af68701
IS Choi <insil321@hotmail.com>
2018-10-03 02:04:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -2,12 +2,11 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
       
-     }
+    
 
 
 end
